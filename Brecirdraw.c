476f53e9d75f69545c30c7ba3991b788d7508eab
Abu-Talha-Choudhury <91937351+Abu-Talha-Choudhury@users.noreply.github.com>
2022-08-07 11:49:06 +0600

    Add files via upload

---
+++ Brecirdraw.c
@@ -0,0 +1,148 @@
+#include <stdio.h>
+
+
+
+
+
+void drawCircle(int xc, int yc, int x, int y)
+
+
+{
+
+
+    printf("(%d, %d) \n",xc+x, yc+y);
+
+
+    printf("(%d, %d) \n",xc-x, yc+y);
+
+
+    printf("(%d, %d) \n",xc+x, yc-y);
+
+
+    printf("(%d, %d) \n",xc-x, yc-y);
+
+
+    printf("(%d, %d) \n",xc+y, yc+x);
+
+
+    printf("(%d, %d) \n",xc-y, yc+x);
+
+
+    printf("(%d, %d) \n",xc+y, yc-x);
+
+
+    printf("(%d, %d) \n",xc-y, yc-x);
+
+
+
+
+
+}
+
+
+
+
+
+void circleBres(int xc, int yc, int r)
+
+
+{
+
+
+    int x = 0, y = r;
+
+
+    int d = 3 - 2 * r;
+
+
+    drawCircle(xc, yc, x, y);
+
+
+    while (y >= x)
+
+
+    {
+
+
+
+
+
+        x++;
+
+
+
+
+
+        if (d > 0)
+
+
+        {
+
+
+            y--;
+
+
+            d = d + 4 * (x - y) + 10;
+
+
+        }
+
+
+        else
+
+
+            d = d + 4 * x + 6;
+
+
+        drawCircle(xc, yc, x, y);
+
+
+
+
+
+    }
+
+
+}
+
+
+
+
+
+
+
+
+int main()
+
+
+{
+
+
+    int x,y,r;
+
+
+
+
+
+    printf("Enter radius of circle: ");
+
+
+    scanf("%d", &r);
+
+
+    printf("Enter co-ordinates of center(x and y): ");
+
+
+    scanf("%d%d", &x, &y);
+
+
+
+
+
+    circleBres(x, y, r);
+
+
+    return 0;
+
+
+}
